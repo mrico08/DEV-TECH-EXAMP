fc423d78e0f75122d74a2732ed7cf4aa064820bf
tpo-sa@mec.ph <m.rico@mec.ph>
2024-05-29 17:41:44 +0800

    commit changes

--- index.php
+++ index.php
@@ -44,6 +44,7 @@
 <body class="g-sidenav-show  bg-gray-100">
   <?php include("page/body/sidebar.php");?>
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+    <?php include("page/body/topbar.php");?>
     <div class="container-fluid py-4">
         <?php
                 if(isset($_GET["page"])){
